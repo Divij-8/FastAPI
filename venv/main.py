532a3a59eb6a4a9ae6e6051a192d53241c55d644
Divij Mazumdar <divij.mazumdar@gmail.com>
2025-10-07 20:49:44 +0530

    Update main.py

--- venv/main.py
+++ venv/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 app=FastAPI()
 @app.get("/")
 def read_root():
-    return {"Hello":"Wdorld"}
+    return {"Hello":"World"}
 @app.get("/hello/{name}")
 def greet_name(name:str):
     return {"greeting":f"Hello, {name}!"}
